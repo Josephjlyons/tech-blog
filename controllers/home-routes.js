f38ae98405cb56a5812d3e8c6cf5baf443036104
Joe Lyons <josephjlyons90@gmail.com>
2021-04-07 13:39:28 -0400

    config connection

---
+++ controllers/home-routes.js
@@ -0,0 +1,15 @@
+const sequelize = require("../config/connection");
+const {Post, User, Comment} = require('../models');
+const router = require('express').Router();
+
+router.get('/', async (req,res) => {
+    
+})
+
+
+
+
+
+
+
+module.exports = router;
