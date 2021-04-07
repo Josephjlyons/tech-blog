55e5105fc1a4f92294102c1192a1a7bc444182d5
Joe Lyons <josephjlyons90@gmail.com>
2021-04-07 16:04:44 -0400

    add dashboard routes & require

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes')
+const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes)
 
 router.use((req, res) => {
     res.send('<h1>Route Not Found</h1>')
