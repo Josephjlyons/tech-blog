a1cc07d7be8230a299ab38159b46ba1b223ee6d2
Joe Lyons <josephjlyons90@gmail.com>
2021-04-08 21:10:33 -0400

    fix error in routes for log out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,65 +12,26 @@ router.get('/', async (req, res) => {
     };
 });
 
-// router.get('/:id', async (req, res) => {
-//     try {
-//         const userData = await User.findOne({
-//             attributes: { exclude: ['password'] },
-//             where: {
-//                 id: req.params.id
-//             },
-//             include: [{
-//                 model: Post,
-//                 attributes: ['id', 'title', 'content', 'created_at'],
-//             },
-//             {
-//                 model: Comment,
-//                 attributes: ['id', 'comment_text', 'created_at'],
-//                 include: {
-//                     model: Post,
-//                     attributes: ['title']
-//                 }
-//             },
-//             {
-//                 model: Post,
-//                 attributes: ['title']
-//             }
-//             ]
-//         }).then(userData => {
-//             if (!userData) {
-//                 res.status(400).json({ message: 'No user found matching this id' });
-//             }
-//             res.status(userData);
-//         })
-//     } catch (err) {
-//         res.status(500).json(err)
-//     };
-// });
-
 router.post('/', async (req, res) => {
     try {
-        const userCreate = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }).then(userCreate => {
-            req.session.save(() => {
-                req.session.user_id = userCreate.id,
-                    req.session.name = userCreate.name,
-                    req.session.loggedIn = true;
-
-                res.json(userCreate)
-            });
-        })
+      const userData = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
     } catch (err) {
-        res.status(500).json(err)
-    };
-});
+      console.log(err)
+      res.status(400).json(err);
+    }
+  });
 
 router.post('/login', async (req, res) => {
     try {
       const userData = await User.findOne({ where: { email: req.body.email } });
-  
+     
       if (!userData) {
         res
           .status(400)
@@ -99,35 +60,35 @@ router.post('/login', async (req, res) => {
     }
   });
 
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
+router.get('/logout', (req, res) => {
+    if (req.session.logged_in) {
         req.session.destroy(() => {
-            res.status(204).end()
+            res.redirect('/').end()
         });
 
     } else {
-        res.status(404).end();
+        res.status(400).end();
     };
 });
 
-router.put('/:id', async (req, res) => {
-    try {
-        const userUpdate = await User.update(req.body, {
-            individualHooks: true,
-            where: {
-                id: req.params.id
-            }
-        }).then(userUpdate => {
-            if (!userUpdate[0]) {
-                res.status(404).json({ message: 'no user found matching this id' });
-                return;
-            }
-            res.json(userUpdate)
-        })
-    } catch (err) {
-        res.status(500).json(err)
-    };
-});
+// router.put('/:id', async (req, res) => {
+//     try {
+//         const userUpdate = await User.update(req.body, {
+//             individualHooks: true,
+//             where: {
+//                 id: req.params.id
+//             }
+//         }).then(userUpdate => {
+//             if (!userUpdate[0]) {
+//                 res.status(404).json({ message: 'no user found matching this id' });
+//                 return;
+//             }
+//             res.json(userUpdate)
+//         })
+//     } catch (err) {
+//         res.status(500).json(err)
+//     };
+// });
 
 router.delete('/:id', async (req, res) => {
     try {
