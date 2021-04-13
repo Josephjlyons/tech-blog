2da692897b60ffdf5e4fc827847eeec162a936aa
Joe Lyons <josephjlyons90@gmail.com>
2021-04-13 17:11:38 -0400

    make seeding data more professional

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,18 +2,18 @@ const { Post } = require('../models');
 
 const postData = [
     {
-        title: "Title For Post One",
-        content: "I am the post content for post number one",
+        title: "Why MVC is so important",
+        content: "MVC offers support for rapid and parallel development. So, developing web applications using the MVC model it is possible that one developer work on the view while the another can work on the controller. This helps for easy implementation of the business logic of the web application.",
         user_id: 1
     },
     {
-        title: "Title For Post Two",
-        content: "I am the post content for post number two",
+        title: "Authentication vs Authorization",
+        content: "Authentication and authorization might sound similar, but they are distinct security processes in the world of identity and access management (IAM). Authentication confirms that users are who they say they are. Authorization gives those users permission to access a resource",
         user_id: 2
     },
     {
-        title: "Title For Post Three",
-        content: "I am the post content for post number three",
+        title: "What is ORM",
+        content: "Object–relational mapping in computer science is a programming technique for converting data between incompatible type systems using object-oriented programming languages. This creates, in effect, a 'virtual object database' that can be used from within the programming language.",
         user_id: 3
     }
 
