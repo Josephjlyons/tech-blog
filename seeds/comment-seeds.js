3f5355f215a2d42d72e6ec7837be66b8f048d7ba
Joe Lyons <josephjlyons90@gmail.com>
2021-04-13 17:33:17 -0400

    rework seed data yet again

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,19 +2,19 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        comment_text: "Comment on post 1 sample",
-        user_id: 1,
+        comment_text: "Wow thanks for sharing... I am glad I came here to check out this post!",
+        user_id: 3,
         post_id: 1
     },
     {
-        comment_text: "Comment on post 2 sample",
-        user_id: 2,
+        comment_text: "Huh, they sound so similiar but are so different. Who would of guessed... certainly not me.",
+        user_id: 1,
         post_id: 2
     },
 
     {
-        comment_text: "Comment on post 3 sample",
-        user_id: 3,
+        comment_text: "Now that I am clear on what it stands for it makes a lot more sense.",
+        user_id: 1,
         post_id: 3
     }
 ];
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,17 +2,17 @@ const { Post } = require('../models');
 
 const postData = [
     {
-        title: "Why MVC is so important",
+        title: "Why MVC is so important?",
         content: "MVC offers support for rapid and parallel development. So, developing web applications using the MVC model it is possible that one developer work on the view while the another can work on the controller. This helps for easy implementation of the business logic of the web application.",
         user_id: 1
     },
     {
-        title: "Authentication vs Authorization",
+        title: "Authentication vs Authorization: What's the difference?",
         content: "Authentication and authorization might sound similar, but they are distinct security processes in the world of identity and access management (IAM). Authentication confirms that users are who they say they are. Authorization gives those users permission to access a resource",
         user_id: 2
     },
     {
-        title: "What is ORM",
+        title: "What is ORM?",
         content: "Object–relational mapping in computer science is a programming technique for converting data between incompatible type systems using object-oriented programming languages. This creates, in effect, a 'virtual object database' that can be used from within the programming language.",
         user_id: 3
     }
