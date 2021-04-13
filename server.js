cab80b2627178809d5c7c53c549cbf753cdcd89c
Joe Lyons <josephjlyons90@gmail.com>
2021-04-13 17:10:03 -0400

    change max age

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: { maxAge: 360000 },
+    cookie: { maxAge: 3600000 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
